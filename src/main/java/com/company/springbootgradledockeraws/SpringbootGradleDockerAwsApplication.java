a0f340a38551afb2f82fe190a236db77017b3ec6
gandalf <gandalf@gandalfs-MacBook-Pro.local>
2021-01-23 18:12:05 -0600

    adding springboot files

---
+++ src/main/java/com/company/springbootgradledockeraws/SpringbootGradleDockerAwsApplication.java
@@ -0,0 +1,13 @@
+package com.company.springbootgradledockeraws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGradleDockerAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGradleDockerAwsApplication.class, args);
+	}
+
+}
