15be104e8dfd71783b16633ad2c6cb3633d5e71d
gandalf <gandalf@gandalfs-MacBook-Pro.local>
2021-04-14 18:22:24 -0500

    for later

--- src/main/java/com/company/springbootgradledockeraws/EmployeeNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.company.springbootgradledockeraws;
-
-import org.json.JSONObject;
-
-public class EmployeeNotFoundException extends RuntimeException {
-    EmployeeNotFoundException(JSONObject jsonObject) {
-        super(String.valueOf(jsonObject));
-    }
-}
